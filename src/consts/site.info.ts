9bc037fbd6050f353b1a6e6dd271aec8947c6fd3
sebahernandez <sebaprogramer@gmail.com>
2025-01-06 16:56:23 -0300

    cambio de imagen de url absoluta

--- src/consts/site.info.ts
+++ src/consts/site.info.ts
@@ -4,6 +4,6 @@ export const siteInfo = {
     "Venta de vehículos usados de las mejores marcas y modelos. Contamos con financiamiento y garantía en todos nuestros vehículos.",
   keywords:
     "venta de autos, venta de vehiculos, venta de usados, compra venta de vehiculos, compra venta de autos",
-  image: "/images/ogg-image.jpeg",
+  image: "https://www.automotiveconsulting.cl/images/ogg-image.jpeg", // URL absoluta
   url: "https://www.automotiveconsulting.cl",
 };
